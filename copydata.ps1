bb8c7c1f57bb632b9adf72959102d6cfb0092ba0
TheJens90 <287219a5@opayq.com>
2016-09-12 18:18:18 +0200

    Added script

---
+++ copydata.ps1
@@ -0,0 +1,12 @@
+﻿$quellordner = "C:\Users\jensb\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+$zielorder = "C:\Users\jensb\Pictures\Hintergrund"
+if (-not (Test-Path $zielorder)) { md $zielorder }
+$dateien = dir $quellordner
+"Kopiere $($dateien.Count) Dateien..."
+foreach($datei in $dateien)
+{
+$zieldatei = "$zielorder\$($datei.name).jpg"
+Copy-Item $datei.FullName $zieldatei
+$zieldatei
+}
+"$($dateien.Count) Bilder kopiert!"
